563df10eddd94ce4f81d109e80163dd783508cfc
Uranium-Deng <dengruizhi1210@163.com>
2022-02-27 11:42:14 +0800

    v0.8_client_server_completed

--- client/client.c
+++ client/client.c
@@ -13,21 +13,34 @@
 
 
 char *client_cfg_file = (char *)"./client.cfg";
+int sockfd;
+char logfile[50] = {0}; // 记录聊天信息的log文件
+
+
+void logout(int signalnum) {
+    close(sockfd);
+    printf("您已退出聊天室!\n");
+    exit(1);
+}
+
 
 int main() {
-    int port, sockfd;
+    int port;
     struct Msg msg;
     char ip[20] = {0};
-    strcpy(ip, get_value(client_cfg_file, (char *)"SERVER_IP"));
     port = atoi(get_value(client_cfg_file, (char *)"SERVER_PORT"));
+    strcpy(ip, get_value(client_cfg_file, (char *)"SERVER_IP"));
+    strcpy(logfile, get_value(client_cfg_file, "LOG_FILE"));
 
     if ((sockfd = socket_connect(ip, port)) < 0) {
+        // 连接server失败
         perror("socket_connect");
         return 1;
     }
     strcpy(msg.from, get_value(client_cfg_file, "MY_NAME"));
     msg.flag = 2;
     if (chat_send(msg, sockfd) < 0) {
+        // 向server第一次发送信息，供server记录本用户
         return 2;
     }
 
@@ -43,24 +56,56 @@ int main() {
     }
 
     pid_t pid;
+    signal(SIGINT, logout);  // 设置logout函数来处理终止信号
     if ((pid = fork()) < 0) {
         perror("fork");
     }
     if (pid == 0) {
         // 子进程，负责消息发送
-        while (1) {
-            printf(L_PINK"Please input message: "NONE"\n");
+        sleep(1);
+        char c = 'a';
+        while (c != EOF) {
+            system("clear");
+            printf(PINK"Please input message: "NONE"\n");
             memset(msg.message, 0, sizeof(msg.message));
             scanf("%[^\n]s", msg.message);
-            getchar(); 
+            c = getchar(); 
+            msg.flag = 0;
+            if (msg.message[0] == '@') {
+                msg.flag = 1;
+            } else if (msg.message[0] == '#') {
+                msg.flag = 4;
+            }
+            if (!strlen(msg.message)) continue;
             chat_send(msg, sockfd);
-            system("clear");
         }
+        close(sockfd);
     } else {
         // 父进程，负责消息接收
-        wait(NULL);
-    }
+        freopen(logfile, "w", stdout);  // 将stdout重定向到logfile中
+        printf(GREEN"Server "NONE": %s\n", rmsg.msg.message);
+        fflush(stdout);
 
+        while (1) {
+            rmsg = chat_recv(sockfd);
+            if (rmsg.retval < 0) {
+                printf("Error in Server!\n");
+                break;
+            }
+            if (rmsg.msg.flag == 0) { // 公聊信息
+                printf(BLUE"%s"NONE": %s\n", rmsg.msg.from, rmsg.msg.message);
+            } else if (rmsg.msg.flag == 1) { // 私聊信息
+                printf(BLUE"%s"L_GREEN"*"NONE": %s\n", rmsg.msg.from, rmsg.msg.message);
+            } else if (rmsg.msg.flag == 2) {  // 服务器通知信息
+                printf(YELLOW"通知信息: " NONE "%s\n", rmsg.msg.message);
+            } else { // 出错
+                printf("Error!\n");
+            }
+            fflush(stdout);
+        }
+        wait(NULL);
+        close(sockfd);
+    }
     return 0;
 }
 
--- server/server.c
+++ server/server.c
@@ -20,7 +20,7 @@ struct User {
 };
 
 char *cfg_file = "./server.cfg";
-
+int online_cnts;  // 记录在线总人数
 struct User *client;
 
 
@@ -43,23 +43,112 @@ int find_free_index() {
     return -1;
 }
 
-/* 线程对应的work函数 */
+/* 向所有在线用户发送信息 */
+void send_all(struct Msg msg) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online != 1) continue;
+        chat_send(msg, client[i].fd);
+    }
+}
+
+/* 向除自己以外的所有在线用户发送信息 */
+void send_all_except_me(struct Msg msg, int idx) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online != 1) continue;
+        if (idx = i) continue;
+        chat_send(msg, client[i].fd);
+    }
+}
+
+/* 判断该私聊对象是否存在且在线，存在且在线则返回数组下标，否则返回-1 */
+int check_name(char *name) {
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if (client[i].online && !strcmp(client[i].name, name))
+            return i;
+    }
+    return -1;
+}
+
+/* 获取当前在线的一部分client姓名 */
+void get_online_client(char *message) {
+    int cnt = 0;
+    char temp[35] = {0};
+    sprintf(message, "当前有 ");
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        if(client[i].online) {
+            if (cnt != 0 ) sprintf(temp, ",%s", client[i].name);
+            else sprintf(temp, "%s", client[i].name);
+            strcat(message, temp);
+            cnt++;
+            if (cnt >= 5) break;
+        } 
+    }
+    sprintf(temp, " 等%d个用户在线", online_cnts);
+    strcat(message, temp);
+}
+
+
+/* 线程对应的work函数,服务端任务：接收+转发*/
 void *work(void *arg) {
-    int *idx = (int *)arg;
-    int client_fd = client[*idx].fd;
+    int idx = *(int *)arg;
+    int client_fd = client[idx].fd;
+    
     struct RecvMsg rmsg;
-    printf(GREEN"Login "NONE": %s\n", client[*idx].name);
+    printf(GREEN"Login "NONE": %s\n", client[idx].name);
+    rmsg.msg.flag = 2; // 系统通知信息2
+    sprintf(rmsg.msg.message, "你的好友 %s 上线了，和他打个招呼吧!!!", client[idx].name);
+    send_all_except_me(rmsg.msg, idx);
+
     while (1) {
         rmsg = chat_recv(client_fd);
-        if (rmsg.retval < 0) {
-            printf(PINK"Logout: "NONE"%s\n", client[*idx].name);
+        if (rmsg.retval < 0) { // client下线
+            printf(PINK"Logout: "NONE"%s\n", client[idx].name);
             close(client_fd);
-            client[*idx].online = 0;
+            client[idx].online = 0;
+            online_cnts--;
+            sprintf(rmsg.msg.message, "好友 %s 已下线.", client[idx].name);
+            rmsg.msg.flag = 2;
+            send_all(rmsg.msg);
             return NULL;
         }
-        printf(BLUE"%s"NONE": %s\n", rmsg.msg.from, rmsg.msg.message);
-    }
 
+        if (rmsg.msg.flag == 0) {  // 公聊信息0
+            printf(BLUE"%s"NONE" : %s\n",rmsg.msg.from, rmsg.msg.message);
+            if (!strlen(rmsg.msg.message)) continue;
+            send_all(rmsg.msg);
+        } else if (rmsg.msg.flag == 1) {  // 私聊信息1
+            if (rmsg.msg.message[0] == '@') {
+                // 获取私聊对象name
+                char to[32] = {0};
+                int i = 1;
+                for (; i <= 32; i++) {
+                    if (rmsg.msg.message[i] == ' ')
+                        break;
+                }
+                strncpy(to, rmsg.msg.message + 1, i - 1);
+                // 判断私聊对象是否在线，若不在则告知client私聊对象不在线
+                int ret = 0;
+                if ((ret = check_name(to)) < 0) {
+                    sprintf(rmsg.msg.message, "%s is not online.", to);
+                    rmsg.msg.flag = 2; 
+                    chat_send(rmsg.msg, client_fd);
+                    continue;
+                } else if (!strlen(rmsg.msg.message + i)) { // 私聊信息为空
+                    sprintf(rmsg.msg.message, "私聊消息不能为空");
+                    rmsg.msg.flag = 2;
+                    chat_send(rmsg.msg, client_fd);
+                    continue;
+                }
+                printf(L_PINK"Note: "NONE" %s 给 %s 发送了一条私密信息\n", rmsg.msg.from, to);
+                chat_send(rmsg.msg, client[idx].fd);
+            }
+        } else if (rmsg.msg.flag == 4 && rmsg.msg.message[0] == '#') {
+            printf(L_PINK"Note: "NONE" %s查询了在线人数\n", rmsg.msg.from);
+            get_online_client(rmsg.msg.message);
+            rmsg.msg.flag = 2;
+            chat_send(rmsg.msg, client_fd);                               
+        }
+    }
     return NULL;
 }
 
@@ -97,18 +186,21 @@ int main() {
             close(fd);
             continue;
         }
-         msg.flag = 2;
-         strcpy(msg.message, "Welcome to this Chatroom!\n");
-         chat_send(msg, fd);
+        msg.flag = 2;
+        strcpy(msg.message, "Welcome to this Chatroom!\n");
+        chat_send(msg, fd);
 
         // 登记用户信息并创建线程为之服务
         int idx = find_free_index();
         client[idx].online = 1;
         client[idx].fd = fd;
+        online_cnts++;
         strcmp(client[idx].name, recvmsg.msg.from);
-        pthread_create(&client[idx].tid, NULL, work, (void *)&idx);
+        int pthread_ret = pthread_create(&client[idx].tid, NULL, work, (void *)&idx);
+        if (pthread_ret) {
+            fprintf(stderr, "pthread_create");
+        }
     }
-
     return 0;
 }
 
